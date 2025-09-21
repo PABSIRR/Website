ba798ac1654336f5f87841af68bab702e7471735
Pavan Gudavalli <elobooster.help@gmail.com>
2025-09-20 21:53:36 -0700

    Vercel Analytics

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import Navbar from './components/Navbar.vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 
 <template>
+  <SpeedInsights/>
   <div id="app">
     <Navbar />
     <main class="page-container">
